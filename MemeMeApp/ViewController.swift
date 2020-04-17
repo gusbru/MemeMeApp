99f6b8bd0e7ac0b171024116597b67df7eafd4f1
Gustavo Brunetto <gustavobruneto@gmail.com>
2020-04-17 16:44:49 -0700

    change label to text field

--- MemeMeApp/ViewController.swift
+++ MemeMeApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var image: UIImageView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
