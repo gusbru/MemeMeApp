aeb72695a98ddf36025b06f1ffbd904271bc3751
Gustavo Brunetto <gustavobruneto@gmail.com>
2020-04-17 17:46:28 -0700

    can open the pick image and cancel

--- MemeMeApp/ViewController.swift
+++ MemeMeApp/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var topLabel: UITextField!
@@ -27,12 +27,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func pickImage(_ sender: Any) {
+        let pickController = UIImagePickerController()
+        pickController.delegate = self
+        present(pickController, animated: true, completion: nil)
     }
     
     @IBAction func takePicture(_ sender: Any) {
+        print("takePicture")
     }
     
     @IBAction func share(_ sender: Any) {
+        // TODO: change this image by an image from the ick
+        let image = UIImage()
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("imagePickController")
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
 
